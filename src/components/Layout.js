fa3f303731a56e31e48dad0551eef06a4041c835
Your Name <you@example.com>
2017-06-06 12:39:35 -0500

    first commit

---
+++ src/components/Layout.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Footer from './Footer';
+
+export default class Layout extends Component {
+  render() {
+    return (
+      <div>
+      	<header>
+			cabeza
+        </header>
+      
+      	<Footer/>
+      		
+
+      </div>
+    );
+  }
+}
+
